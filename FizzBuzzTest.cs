ad533f008d0f234815cdb6fc3c3ad6d68e2355e6
Martin Gondermann <magicmonty@pagansoft.de>
2013-07-14 11:34:39 +0200

    Second Test

--- FizzBuzzTest.cs
+++ FizzBuzzTest.cs
@@ -17,10 +17,26 @@ namespace FizzBuzzKata
         public void ShouldReturnFizzForValuesDivisibleBy3()
         {
             var value = 3;
+
             var result = string.Empty;
             if (value % 3 == 0)
                 result = "Fizz";
+
             Assert.That(result, Is.EqualTo("Fizz"));
+        }
+
+        [Test]
+        public void ShouldReturnBuzzForValuesDivisibleBy5()
+        {
+            var value = 5;
+
+            var result = string.Empty;
+            if (value % 3 == 0)
+                result = "Fizz";
+            if (value % 5 == 0)
+                result = "Buzz";
+
+            Assert.That(result, Is.EqualTo("Buzz"));
         }
     }
 }
